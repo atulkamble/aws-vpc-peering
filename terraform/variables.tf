23806151042acd28d49c36a8e6f15f30161e063a
Atul Kamble <atul_kamble@hotmail.com>
2025-07-16 15:49:01 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,10 +12,10 @@ variable "instance_type" {
 
 variable "ami_id" {
   description = "Amazon Linux 2 AMI"
-  default     = "ami-0c02fb55956c7d316" # Update as needed
+  default     = "ami-0c02fb55956c7d316"
 }
 
 variable "key_name" {
-  description = "Name of the EC2 key pair to use for SSH"
-  default     = "my-key" # <-- Change to your actual AWS key name
+  description = "Name of the EC2 key pair for SSH access"
+  default     = "my-key" # <-- Replace with your actual key name
 }
