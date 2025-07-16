01ab078619e0cd87331ebf66870f50f69b8bdc55
Atul Kamble <atul_kamble@hotmail.com>
2025-07-16 15:51:15 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,17 @@
-output "ec2_a_private_ip" {
-  value = aws_instance.ec2_a.private_ip
-}
-
-output "ec2_b_private_ip" {
-  value = aws_instance.ec2_b.private_ip
-}
-
 output "ec2_a_public_ip" {
   value = aws_instance.ec2_a.public_ip
 }
 
 output "ec2_b_public_ip" {
   value = aws_instance.ec2_b.public_ip
+}
+
+output "ec2_a_private_ip" {
+  value = aws_instance.ec2_a.private_ip
+}
+
+output "ec2_b_private_ip" {
+  value = aws_instance.ec2_b.private_ip
 }
 
 output "vpc_peering_connection_id" {
